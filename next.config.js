64733236429db0f04a1c1af076d713805eb746bd
open24hrs <134480158+open24hrs@users.noreply.github.com>
2024-09-03 01:39:06 -0500

    1

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  // Add any other Next.js configuration options here
+	reactStrictMode: true,
+	pwa: {
+		dest: "public",
+		register: true,
+		skipWaiting: true,
+		runtimeCaching,
+		buildExcludes: [/middleware-manifest.json$/]
+	}
 });
