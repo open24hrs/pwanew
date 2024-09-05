1941012103dc115569c6fcf1985dc249f65555ee
open24hrs <134480158+open24hrs@users.noreply.github.com>
2024-09-05 06:45:08 -0500

    1

--- next.config.js
+++ next.config.js
@@ -8,10 +8,10 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "production",
-}); // Add closing parenthesis here
+  disable: process.env.NODE_ENV !== "production", // Enable PWA in production
+});
 
 module.exports = withPWA({
   reactStrictMode: true,
   // Add any other Next.js configuration options here
-}); // Add closing curly brace here
+});
